f529f89aadb459a1e33206de54ce1dc26d9bf892
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2017-04-22 15:50:19 -0400

    adding lat and long and first request objects

---
+++ src/main.py
@@ -0,0 +1,6 @@
+import skyinterface
+
+if __name__ == "__main__":
+    lucien = skyinterface.User(latitude=42.7284,longitude=73.6918)
+
+    print lucien.clear_dark_sky.getJSON()
--- src/skyinterface.py
+++ src/skyinterface.py
@@ -1,7 +1,11 @@
 import astropy
+import astropy.coordinates
+from astropy.coordinates import EarthLocation
+import os
+import requests
 
 class Astronomer:
-    location;#astropy.coordinates.AltAz()
+    #astropy.coordinates.AltAz()
     def __init__(self,location):
         self.location = location
 
@@ -9,7 +13,9 @@ class Astronomer:
         return time
 
     def getObject(self,name):
-        ##Should accept Messier designation, NGC, and proper noun name.
+        ##Perhaps get name to astropyname database going in dynamoDB in AWS?
+        ##for now, astropy should be good enough for most objects.
+        ##Should accept Messier designation, NGC, and proper noun name. #perhaps interface with a
         return string#returns the astropy object.
 
     def getRADEC(self,obj):
@@ -25,43 +31,77 @@ class Astronomer:
         return (astrobject1,astrobject2)##A bright star and a
 
 
-import os
 ##cleardarksky class.
 ##No inheritance, because we need to get things done fast.
 ##has a reference to a user, because we need latitude and longitude.
 ##returns all the weather/seeing data from cleardarksky.
 class ClearDarkSky:
-
+    #Cache data for the past hour.
+    ##api limits to 1000 api calls.
     def __init__(self,location):
         self.location = location
         self.api_key = os.getenv("DARK_SKY_API_KEY",'')
         self.url = "https://api.darksky.net/forecast/"
-    /37.8267,-122.4233
-    def getJSON():
-        url = self.url+self.api_key+"/"+self.location.latitude()+","+self.location.longitude()
 
-        
+        self.cachedJSON = ""
+
+    def getJSON(self):
+        url = self.url+self.api_key+"/"+self.location.latitude()+","+self.location.longitude()
+        r = requests.get(url)
+        print r
+
 
     def getCloudCover():
+        ##caching attempt, but now ignore this.
+        json = getJSON()
+        data = json["hourly"]["data"]
+        cloud_cover = data["cloudCover"]
+        return cloud_cover
 
     def getTransparency():
+        json = getJSON()
+        data = json["hourly"]["data"]
+        transparency = data["transparency"]
+        return transparency
 
     def getSeeing():
+        pass
 
     def getDarkness():
+        pass
 
     def getWind():
+        pass
 
     def getHumidity():
+        pass
 
     def getTemperature():
+        pass
 
     def apiCall():
-
+        pass
 
 
 class User:
-    latitude = ""
-    longitude = ""
-    clear_sky_chart = ClearSkyChart()
-    astronomer = Astronomer()
+    def __init__(self,longitude,latitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+        earthloc = EarthLocation(longitude,latitude,20)
+        print earthloc
+        self.location = earthloc.from_geodetic(longitude,latitude,20)
+        print "latitude:",self.location.latitude
+        print "longitude:",self.location.longitude
+
+        self.clear_dark_sky = ClearDarkSky(self.location)
+        self.astronomer = Astronomer(self.location)
+
+    ##wrapper classes called by ALEXA
+    #weather classes
+    #def getCloudCover():
+        #
+
+    #...
+    #wrapper classes for astronomer
+    ##
