f1e94a618d9483e9d684164bf074c329284a1f3b
lucien christie-dervaux <lucienchristiedervaux@gmail.com>
2017-04-22 14:01:04 -0400

    Added class skeletons for other classes, rearranged src

---
+++ src/skyinterface.py
@@ -0,0 +1,61 @@
+import astropy
+
+class Astronomer:
+    location;#astropy.coordinates.AltAz()
+    def __init__(self,location):
+        self.location = location
+
+    def getTime(self):
+        return time
+
+    def getObject(self,name):
+        ##Should accept Messier designation, NGC, and proper noun name.
+        return string#returns the astropy object.
+
+    def getRADEC(self,obj):
+        return RADEC#Ra Dec astropy object.
+
+    def getAltAz(self,obj):
+        return ALTAZ#altaz astropy object
+
+    def getClosestParallel(self,obj):
+        return astrobject
+
+    def getRandomParallel(self):
+        return (astrobject1,astrobject2)##A bright star and a
+
+
+
+##cleardarksky class.
+##No inheritance, because we need to get things done fast.
+##has a reference to a user, because we need latitude and longitude.
+##returns all the weather/seeing data from cleardarksky.
+class ClearDarkSky:
+
+    def __init__(self,location):
+        self.location = location
+
+
+    def getCloudCover():
+
+    def getTransparency():
+
+    def getSeeing():
+
+    def getDarkness():
+
+    def getWind():
+
+    def getHumidity():
+
+    def getTemperature():
+
+    def apiCall():
+
+
+
+class User:
+    latitude = ""
+    longitude = ""
+    clear_sky_chart = ClearSkyChart()
+    astronomer = Astronomer()
